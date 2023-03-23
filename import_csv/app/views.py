2337b3fc21716ad3c02c9889af9139e743d2b795
nurtaufik2020 <nurtaufik2020@gmail.com>
2023-03-24 03:01:49 +0700

    24-03-2023

---
+++ import_csv/app/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from tablib import Dataset
+from .resources import PersonResource
+from .models import Person
+# Create your views here.
+def index(request):
+    return render(request,'base.html')
+
+def importExcel(request):
+    if request.method == 'POST':
+        person_resource= PersonResource()
+        dataset=Dataset()
+        new_persons= request.FILES['my_file']    
+        imported_data = dataset.load(new_persons.read(),format='xlsx')
+        for data in imported_data:
+            value = Person(
+                data[0],
+                data[1],
+                data[2]
+            )
+            value.save()
+            
+    return render(request,'dashboard/form.html')
